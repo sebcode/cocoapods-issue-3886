43b48da7e75c740fc51683458e51106059f6f047
Sebastian Volland <seb@baunz.net>
2015-07-25 10:59:03 +0300

    Initial Commit

---
+++ cocoapodstest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  cocoapodstest
+//
+//  Created by Sebastian Volland on 25/07/15.
+//  Copyright © 2015 asdf. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ cocoapodstest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  cocoapodstest
+//
+//  Created by Sebastian Volland on 25/07/15.
+//  Copyright © 2015 asdf. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
